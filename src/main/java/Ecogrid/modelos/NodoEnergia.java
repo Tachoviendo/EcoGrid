ddecf01051602d91ca437e2675186550360a908a
aristov983 <earis4499@gmail.com>
2026-05-07 16:28:44 -0300

    nodo energia terminado

--- src/main/java/Ecogrid/modelos/NodoEnergia.java
+++ src/main/java/Ecogrid/modelos/NodoEnergia.java
@@ -1,22 +1,65 @@
 package Ecogrid.modelos;
 
 public class NodoEnergia extends EntidadRed {
-    public static int contador = 0; //compartido por todas las instancias.
-    //Atributos
+
+    public static int contador = 0;
+
+    // Atributos
     private final double capacidadMaxima;
     private double cargaActual;
 
-    //TODO: Consumidores Permitidos.
-    
+    // Consumidores permitidos (no quiero ni saber lo que nos va a costar esto)
+    private ListaEnlazada<Consumidor> consumidoresPermitidos;
 
-    public double getCapacidadDisponible(){
+    // Constructor
+    public NodoEnergia(double capacidadMaxima) {
+        this.capacidadMaxima = capacidadMaxima;
+        this.cargaActual = 0;
+        this.consumidoresPermitidos = new ListaEnlazada<>();
+
+        contador++;
+        this.setId(contador);
+    }
+
+    // Getters
+
+    public double getCapacidadMaxima() {
+        return capacidadMaxima;
+    }
+
+    public double getCargaActual() {
+        return cargaActual;
+    }
+
+    public ListaEnlazada<Consumidor> getConsumidoresPermitidos() {
+        return consumidoresPermitidos;
+    }
+
+    // Setters
+
+    public void setCargaActual(double cargaActual) {
+        this.cargaActual = cargaActual;
+    }
+
+    public void setConsumidoresPermitidos(ListaEnlazada<Consumidor> consumidoresPermitidos) {
+        this.consumidoresPermitidos = consumidoresPermitidos;
+    }
+
+    // Métodos
+
+    public double getCapacidadDisponible() {
         return this.capacidadMaxima - this.cargaActual;
     }
 
-    //TODO: ID ¿Automatico o Fijo en constructor? ¿Asignaion de consumidores permitidos?
-    public NodoEnergia(double capacidadMaxima){
-        this.capacidadMaxima = capacidadMaxima;
-        contador++;
-        this.setId(contador);
+    public void agregarConsumidorPermitido(Consumidor consumidor) {
+        consumidoresPermitidos.agregarAlFinal(consumidor);
+    }
+
+    public void eliminarConsumidorPermitido(int idConsumidor) {
+        consumidoresPermitidos.eliminarPorId(idConsumidor);
+    }
+
+    public boolean consumidorPermitido(int idConsumidor) {
+        return consumidoresPermitidos.buscarPorId(idConsumidor) != null;
     }
 }
--- src/main/java/Ecogrid/modelos/Transaccion.java
+++ src/main/java/Ecogrid/modelos/Transaccion.java
@@ -1,18 +1,57 @@
 package Ecogrid.modelos;
-//TODO
+
 import java.time.LocalDateTime;
 
-//Atributos
 public class Transaccion {
+
+    // Atributos
     private String idTransaccion;
     private String idNodo;
     private String idConsumidor;
     private Double cantidadEnergia;
     private LocalDateTime timeStamp;
 
-//Getters - Setters
-public void setIdTransaccion(String idTransaccion){
-    this.idTransaccion = idTransaccion;
-}
+    // Getters
 
+    public String getIdTransaccion() {
+        return idTransaccion;
+    }
+
+    public String getIdNodo() {
+        return idNodo;
+    }
+
+    public String getIdConsumidor() {
+        return idConsumidor;
+    }
+
+    public Double getCantidadEnergia() {
+        return cantidadEnergia;
+    }
+
+    public LocalDateTime getTimeStamp() {
+        return timeStamp;
+    }
+
+    // Setters
+
+    public void setIdTransaccion(String idTransaccion) {
+        this.idTransaccion = idTransaccion;
+    }
+
+    public void setIdNodo(String idNodo) {
+        this.idNodo = idNodo;
+    }
+
+    public void setIdConsumidor(String idConsumidor) {
+        this.idConsumidor = idConsumidor;
+    }
+
+    public void setCantidadEnergia(Double cantidadEnergia) {
+        this.cantidadEnergia = cantidadEnergia;
+    }
+
+    public void setTimeStamp(LocalDateTime timeStamp) {
+        this.timeStamp = timeStamp;
+    }
 }
